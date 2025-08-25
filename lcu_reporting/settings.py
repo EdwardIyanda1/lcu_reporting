5cd710db65562d99bc4452bea8f3ba78087a141d
Edward Iyanda <edwardiyanda60@gmail.com>
2025-08-25 17:07:51 +0100

    Add Render domain to ALLOWED_HOSTS

--- lcu_reporting/settings.py
+++ lcu_reporting/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-cxg!zlg4c=e%y!5vcbrwj94rp=##gm7%oj#ec(&9167gc85%x0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "lcu-reporting.onrender.com"]
 
 
 # Application definition
