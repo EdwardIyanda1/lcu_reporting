4e3dd4d3a7c9a230691169905ef638a4122b0b4c
Edward Iyanda <edwardiyanda60@gmail.com>
2025-08-25 17:12:10 +0100

    healthz

--- lcu_reporting/urls.py
+++ lcu_reporting/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def healthz(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+     path("healthz", healthz),
     path('admin/', admin.site.urls),
     path('', include('reports.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
